23452b61ee3f138ec1671fb1e8b75c7628c8755a
hanshugnn <hansjuh77@outlook.com>
2026-09-13 14:01:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import html from "./index.html";
+
 export default {
-  async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+  async fetch(request) {
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    });
   },
 };
